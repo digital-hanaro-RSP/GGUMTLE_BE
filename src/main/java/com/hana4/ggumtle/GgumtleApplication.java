8fb07415cf58d3a1ae698d5ada81e4d73762d109
inwoo <ninw0205@gmail.com>
2025-01-07 13:57:11 +0900

    first commit

---
+++ src/main/java/com/hana4/ggumtle/GgumtleApplication.java
@@ -0,0 +1,13 @@
+package com.hana4.ggumtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgumtleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgumtleApplication.class, args);
+    }
+
+}
