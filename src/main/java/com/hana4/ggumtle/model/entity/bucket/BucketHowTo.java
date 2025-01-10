8c46fb3d9aad4d7bda3bf136517563a9223a325e
문서아 <111281798+mseoa@users.noreply.github.com>
2025-01-10 14:11:30 +0900

    Revert "[feat]엔티티생성#4"

--- src/main/java/com/hana4/ggumtle/model/entity/bucket/BucketHowTo.java
+++
@@ -1,8 +0,0 @@
-package com.hana4.ggumtle.model.entity.bucket;
-
-/**
- * 이루기 위해 무엇이 필요한가요?
- */
-public enum BucketHowTo {
-    MONEY,EFFORT,WILL
-}
--- src/main/java/com/hana4/ggumtle/model/entity/post/PostType.java
+++
@@ -1,4 +0,0 @@
-package com.hana4.ggumtle.model.entity.post;
-
-public enum PostType {
-}
