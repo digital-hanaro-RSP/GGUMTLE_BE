11cd58cb767f14a76769849af425392d76c3cc21
DOEABLE <kimdh090588@gmail.com>
2025-01-10 18:42:47 +0900

    [feat]group관련 dto 파일생성#15

--- src/main/java/com/hana4/ggumtle/model/dto/GroupResponseDto.java
+++ src/main/java/com/hana4/ggumtle/model/dto/GroupResponseDto.java
@@ -1,10 +1,5 @@
 package com.hana4.ggumtle.model.dto;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -12,11 +7,11 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
-public class GroupResponseDto extends BaseDto{
-    private Long id;
-    private String name;
-    private String category;
-    private String description;
-    private String imageUrl;
-    private int memberCount;
+public class GroupResponseDto extends BaseDto {
+		private Long id;
+		private String name;
+		private String category;
+		private String description;
+		private String imageUrl;
+		private int memberCount;
 }
