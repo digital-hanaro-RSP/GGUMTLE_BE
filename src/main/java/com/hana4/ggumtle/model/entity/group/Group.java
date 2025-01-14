0da97e28ef63aab4d2d27aa3a58b3f54ef4d58fd
DOEABLE <kimdh090588@gmail.com>
2025-01-14 10:45:21 +0900

    [feat]꿈모임 api (그룹 조회, 모든 그룹 조회, 커뮤니티 가입)#15

--- src/main/java/com/hana4/ggumtle/model/entity/group/Group.java
+++ src/main/java/com/hana4/ggumtle/model/entity/group/Group.java
@@ -14,14 +14,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 @Builder
 @Entity
 @Table(name = "`Group`")
