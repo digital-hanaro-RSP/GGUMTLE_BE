552b6c889d24649e476cbad0be216d841b46054f
mseoa <0527sophie@naver.com>
2025-01-16 14:23:04 +0900

    [chore] 로그 추가

--- src/main/java/com/hana4/ggumtle/service/CustomUserDetailsService.java
+++ src/main/java/com/hana4/ggumtle/service/CustomUserDetailsService.java
@@ -10,16 +10,22 @@ import com.hana4.ggumtle.repository.UserRepository;
 import com.hana4.ggumtle.security.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 	private final UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// User가 존재하지 않으면 예외를 던지고, 해당 아이디를 로그로 남깁니다.
 		User user = userRepository.findById(username)
-			.orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다. 아이디 : " + username));
+			.orElseThrow(() -> {
+				log.warn("해당 유저가 존재하지 않습니다.(만료된 토큰) 아이디 : {}", username);
+				return new UsernameNotFoundException("해당 유저가 존재하지 않습니다. 아이디 : " + username);
+			});
 		return new CustomUserDetails(user);
 	}
 }
