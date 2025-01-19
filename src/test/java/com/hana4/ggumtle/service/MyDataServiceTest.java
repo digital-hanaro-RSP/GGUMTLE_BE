d9fe08316337b63a1065ba438f80c0a1a34ce328
inwoo <ninw0205@gmail.com>
2025-01-20 01:10:22 +0900

    [test] MyDataServiceTest 커버리지 완료

--- src/test/java/com/hana4/ggumtle/service/MyDataServiceTest.java
+++ src/test/java/com/hana4/ggumtle/service/MyDataServiceTest.java
@@ -1,9 +1,12 @@
 package com.hana4.ggumtle.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.hana4.ggumtle.global.error.CustomException;
+import com.hana4.ggumtle.global.error.ErrorCode;
 import com.hana4.ggumtle.model.entity.myData.MyData;
 import com.hana4.ggumtle.model.entity.user.User;
 import com.hana4.ggumtle.repository.MyDataRepository;
@@ -67,6 +72,49 @@ class MyDataServiceTest {
 		assertTrue(isValidAmount(result.getForeignCurrency()));
 		assertTrue(isValidAmount(result.getPension()));
 		assertTrue(isValidAmount(result.getEtc()));
+	}
+
+	@Test
+	void getMyData_성공() {
+		MyData myData = new MyData();
+		User user = new User();
+		user.setId("1");
+		myData.setUser(new User());
+		myData.setDepositWithdrawal(BigDecimal.ONE);
+		myData.setSavingTimeDeposit(BigDecimal.ONE);
+		myData.setInvestment(BigDecimal.ONE);
+		myData.setForeignCurrency(BigDecimal.ONE);
+		myData.setPension(BigDecimal.ONE);
+		myData.setEtc(BigDecimal.ONE);
+
+		when(myDataRepository.findByUserId("1")).thenReturn(Optional.of(myData));
+
+		assertThat(myDataService.getMyDataByUserId("1").getDepositWithdrawalRatio()).isEqualTo(
+			myData.getDepositWithdrawal().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+		assertThat(myDataService.getMyDataByUserId("1").getSavingTimeDepositRatio()).isEqualTo(
+			myData.getSavingTimeDeposit().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+		assertThat(myDataService.getMyDataByUserId("1").getInvestmentRatio()).isEqualTo(
+			myData.getInvestment().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+		assertThat(myDataService.getMyDataByUserId("1").getForeignCurrencyRatio()).isEqualTo(
+			myData.getForeignCurrency().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+		assertThat(myDataService.getMyDataByUserId("1").getPensionRatio()).isEqualTo(
+			myData.getPension().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+		assertThat(myDataService.getMyDataByUserId("1").getEtcRatio()).isEqualTo(
+			myData.getEtc().divide(BigDecimal.valueOf(6), 4, RoundingMode.HALF_UP));
+	}
+
+	@Test
+	void getMyData_실패_사용자MyData없음() {
+		User user = new User();
+		user.setId("1");
+
+		CustomException exception = assertThrows(CustomException.class, () -> {
+			myDataService.getMyDataByUserId(user.getId());
+		});
+
+		assertEquals(ErrorCode.NOT_FOUND, exception.getErrorCode());
+		assertEquals("해당 유저의 MyData가 연결되지 않았습니다.", exception.getMessage());
+		verify(myDataRepository).findByUserId(user.getId());
 	}
 
 	private boolean isValidAmount(BigDecimal amount) {
