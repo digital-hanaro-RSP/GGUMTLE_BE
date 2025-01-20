edd79c5936570764d1f30defed59ebbe04349464
mseoa <0527sophie@naver.com>
2025-01-20 11:04:07 +0900

    [fix] 클라이언트 요구사항에 따라 서베이 요청시 answers 배열 request에서 제외

--- src/main/java/com/hana4/ggumtle/model/entity/survey/Survey.java
+++ src/main/java/com/hana4/ggumtle/model/entity/survey/Survey.java
@@ -1,14 +1,12 @@
 package com.hana4.ggumtle.model.entity.survey;
 
-import java.util.List;
-
 import com.hana4.ggumtle.model.entity.BaseEntity;
 import com.hana4.ggumtle.model.entity.user.User;
-import com.hana4.ggumtle.util.StringListConverter;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -36,7 +34,7 @@ public class Survey extends BaseEntity {
 	@JoinColumn(name = "userId", nullable = false, foreignKey = @ForeignKey(name = "fk_Survey_userId_User"))
 	private User user;
 
-	@Column(columnDefinition = "TEXT", nullable = false)
-	@Convert(converter = StringListConverter.class)
-	private List<Integer> answers;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private SurveyType surveyType;
 }
