0aeada9ec8867a1d5ccf66ca409898d955bd5230
inwoo <ninw0205@gmail.com>
2025-01-22 14:10:45 +0900

    [fix] 댓글이 있을 때 Post가 삭제 안되는 것 수정

--- src/main/java/com/hana4/ggumtle/model/entity/comment/Comment.java
+++ src/main/java/com/hana4/ggumtle/model/entity/comment/Comment.java
@@ -1,10 +1,12 @@
 package com.hana4.ggumtle.model.entity.comment;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.hana4.ggumtle.model.entity.BaseEntity;
 import com.hana4.ggumtle.model.entity.post.Post;
 import com.hana4.ggumtle.model.entity.user.User;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
@@ -32,7 +34,8 @@ public class Comment extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(cascade = CascadeType.REMOVE)
+	@ManyToOne
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JoinColumn(name = "postId", nullable = false, foreignKey = @ForeignKey(name = "fk_Comment_postId_Post"))
 	private Post post;
 
